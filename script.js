57fa07e470e13558cf7e6f53f641a8594ed184a4
23228cb-colab <23228cb@hvhs.school.nz>
2026-06-10 15:14:48 +1200

    auth

---
+++ script.js
@@ -0,0 +1,27 @@
+/***************************************************************/
+//Google sign in
+/**************************************************************/
+var GLOBAL_user;  // Google's user object
+ function fb_authenticate(){
+    // authenticate with Google
+    firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
+function fb_popupLogin() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user;  // Save the user details object to a global variable
+    console.log("User has logged in")
+  });
+}
+function fb_handleLogin(_user) {
+    if (_user) {
+        GLOBAL_user = _user;//Save the user details object to a global variable
+        console.log(_user.displayName+" is logged in")
+
+    } else {
+        console.log("User is NOT logged in - Starting the popup process")
+        fb_popupLogin();
+    }
+}
+/**************************************************************/
---
+++ setup.js
@@ -0,0 +1,18 @@
+//web app config
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNpwTMFR2I7zZyfrVnH6RHpJuN8IMt1J4",
+  authDomain: "comp-2026-cedric-bell.firebaseapp.com",
+  databaseURL: "https://comp-2026-cedric-bell-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "comp-2026-cedric-bell",
+  storageBucket: "comp-2026-cedric-bell.firebasestorage.app",
+  messagingSenderId: "752102547717",
+  appId: "1:752102547717:web:3bead25f4338c55d37176c"
+};
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ // This log prints the firebase object to the console to show that it is working.
+  // As soon as you have the script working, delete this log.
+  console.log("Firebase initialize finished:");
+ 
