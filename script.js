e124ea5d730ea94a77e24dea5e4c422e7410d8fd
23228cb-colab <23228cb@hvhs.school.nz>
2026-06-23 11:54:05 +1200

    commit+pusher

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
 const UID = localStorage.getItem('UId')
-function pageLoad(){
-if(UID===0){
-    document.getElementById('logout').style.display='none';
-}
-else{
-    document.getElementById("authButton").style.display= 'none';
-};
-if(UID=== null){
-    console.log('it is null')
-    document.getElementById('logOut').style.display='none';
-}
-else{
-    document.getElementById("authButton").style.display= 'none';
-};
-};
-function logOut(){
-     localStorage.setItem('UId', null)
-     console.log(UID)
-     pageLoad()
-}
+
